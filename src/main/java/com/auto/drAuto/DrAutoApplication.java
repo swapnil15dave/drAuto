7095911826293eb64d67d2945bf43ba87fbaa422
swapnil15dave <swapnil15dave@gmail.com>
2021-04-03 18:05:33 +0530

    adding files

---
+++ src/main/java/com/auto/drAuto/DrAutoApplication.java
@@ -0,0 +1,13 @@
+package com.auto.drAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrAutoApplication.class, args);
+	}
+
+}
